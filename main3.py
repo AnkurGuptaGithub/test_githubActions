89ce44d2da81b61ceb471989159bf7c186056703
AnkurGuptaGithub <58750378+AnkurGuptaGithub@users.noreply.github.com>
2025-05-17 12:42:00 +0530

    Update main3.py

--- main3.py
+++ main3.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import time
+from datetime import datetime
+from datetime import date
+import os
 
-bot_token= '7731248996:AAFKGHP-xBCYt1BxlcOLYFnk7FaziSUN9J4'
-chat_id= '1113954519'
+bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+chat_id = os.getenv('CHAT_ID')
 
 # url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
 # requests.get(url).json()
@@ -22,35 +25,105 @@ def web_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
+
+def convert_date(date_str):
+    try:                       return datetime.strptime(date_str+'-2025', "%d-%b-%Y").date()
+    except ValueError:         return None  # Handle invalid date formats if needed
+
+
 driver = web_driver()
-driver.get("https://www.investorgain.com/report/live-ipo-gmp/331/current/")
-time.sleep(15)
+driver.get("https://www.investorgain.com/report/live-ipo-gmp/331/all/")
+time.sleep(10)
 
 body = driver.find_element(By.TAG_NAME, "body")
 
 soup = BeautifulSoup( body.get_attribute("outerHTML") , "html.parser")
-table = soup.find("table", class_= "report-main-table w-auto table table-striped table-bordered table-hover" )  # soup.find("table")  # Adjust the tag if necessary
-print("body:", body, " soup :")
-print(len(soup.find_all("table")))
+table = soup.find("table", class_= "table table-striped table-bordered w-auto" )  # soup.find("table")  # Adjust the tag if necessary
 
-
-print("tables: " ,soup.find_all("table"))
-
-if not table: print("Table not found in the element.")
+if not table: 
+  req= requests.get(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text=Table Not Found").json()
+  exit()
     # Extract headers
-headers = [th.text.strip() for th in table.find_all("th")]
+headers = [  ''.join(filter(str.isalnum, th.text.strip()))  for th in table.find_all("th")]
 
 # Extract rows
 rows = []
 for row in table.find_all("tr")[1:]:  # Skip the header row
-    rows.append([td.text.strip() for td in row.find_all("td")])
+    temp_row = [td.text.strip() for td in row.find_all("td")]
+    if len(temp_row) > 1  : rows.append( temp_row ) 
 
 # Create the DataFrame
-df = pd.DataFrame(rows, columns=headers)
+df = pd.DataFrame(rows, columns=headers[:13])
 
-df.to_csv("backup.csv")
+if len(df)<1:
+  req = requests.get(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text=Table Not Found").json()
+  exit()
 
-message= f"{len(df)}"
+message= ''
+df = df[~df.Status.str.contains('Upcoming')]
+df.columns = df.columns.str.replace(' ', '')
+df.GMP = df.GMP.str.replace('--','0').replace('NA','0').astype('float')
+df.Price = df.Price.str.replace('--','0').replace('NA','0').astype('float')
+df['Close'] = df['Close'].apply(convert_date)
+df['BoADt'] = df['BoADt'].apply(convert_date)
+df['Listing'] = df['Listing'].apply(convert_date)
+
+try:
+  apply = pd.read_csv('apply.csv',index_col=0)
+except:
+  apply = pd.DataFrame(columns= df.columns)
+
+close = df[ df.Close==date.today() ][ df.IPO.isin(apply.IPO) ].reset_index(drop=True)
+
+if not close.empty: message+= "Close \n"
+for i in range(len(close)):
+  message +=   "{}".format(close["IPO"][i]) + " @" + "{}".format(close["EstListing"][i]) + "\n"
+
+listings = df[ df.Listing==date.today() ][ df.IPO.isin(apply.IPO) ].reset_index(drop=True)
+
+if not listings.empty: message+= "Listings \n"
+for i in range(len(listings)):
+  message +=   "{}".format(listings["IPO"][i]) + " @" + "{}".format(listings["EstListing"][i]) + "\n"
+
+allots = df[df.BoADt==date.today()][df.IPO.isin(apply.IPO)].reset_index(drop=True)
+
+if not allots.empty: message+= "Allotment \n"
+for i in range(len(allots)):
+  message +=   "{}".format(allots["IPO"][i]) + " @" + "{}".format(allots["EstListing"][i]) + "\n"
+
+df = df.join(apply.set_index('IPO'), on='IPO', rsuffix='_apply').reset_index(drop=True)
+
+newIPO = df[ df.Status.str.contains('Open') ][ ~df.IPO.isin(apply.IPO) ][ (~df.IPO.str.contains('SME') & (df.GMP/df.Price>0.2)) | 
+ (df.IPO.str.contains('SME') & (df.GMP/df.Price>0.3)) | 
+  (df.FireRating.str.count('🔥') >2)].reset_index(drop=True)
+
+if not newIPO.empty: message+= "New IPOs \n"
+for i in range(len(newIPO)):
+  message +=   "{}".format(newIPO["IPO"][i]) + " @" + "{}".format(newIPO["EstListing"][i]) + "\n"
+
+trending = df[ df.Status.str.contains('Open') ][df.IPO.isin(apply.IPO)][(((df.GMP-df.GMP_apply.astype(float))/df.Price)>0.1 ) | 
+  (df.FireRating.str.count('🔥') >2) ].reset_index(drop=True)
+
+if not trending.empty: message+= "Trending \n"
+for i in range(len(trending)):
+  message +=   "{}".format(trending["IPO"][i]) + " @" + "{}".format(trending["EstListing"][i]) + "\n"
+
+apply = apply[ ~apply.IPO.isin( df[df.IPO.isin(apply.IPO)][((df.GMP_apply.astype(float) - df.GMP)/df.Price)>0.1 ].IPO )].reset_index(drop=True)
+
+apply = pd.concat([apply, newIPO], ignore_index=True, join="inner").reset_index(drop=True)
+
+apply.to_csv('apply.csv')
 
 url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
 req= requests.get(url).json()
+
+
+with open('check_file.txt', 'r') as f:
+    content = f.read()
+content+=1
+with open('check_file.txt', 'w') as f:
+    f.write(content)
+
+print("File edited successfully!")
+
+
